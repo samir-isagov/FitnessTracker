339ae424dfe778ea5b5df57b48ac4648f92fc7da
Samir Isagov <samir.isagov@hotmail.com>
2020-09-03 23:43:31 +0400

    Add DbSets for models

--- FitnessTracker.EFDataAccess/DataAccess/FitnessTrackerContext.cs
+++ FitnessTracker.EFDataAccess/DataAccess/FitnessTrackerContext.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FitnessTracker.EFDataAccess.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace FitnessTracker.EFDataAccess.DataAccess
 {
     public class FitnessTrackerContext : DbContext
     {
-        public FitnessTrackerContext(DbContextOptions options) : base(options) { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseOracle(@"Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=192.168.78.128)(PORT=1521)))(CONNECT_DATA=(SID=orcl)));User ID=FITNESS_TRACKER;Password=123");
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<LibExerciseState> LibExerciseStates { get; set; }
+        public DbSet<LibExercise> LibExercises { get; set; }
+        public DbSet<FinishedExercise> FinishedExercises { get; set; }
     }
 }
--- FitnessTracker.WebAPI/Startup.cs
+++ FitnessTracker.WebAPI/Startup.cs
@@ -20,10 +20,7 @@ namespace FitnessTracker.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<FitnessTrackerContext>(options =>
-            {
-                options.UseOracle(Configuration.GetConnectionString("FitnessTracker"));
-            });
+            services.AddDbContext<FitnessTrackerContext>();
 
             services.AddControllers();
         }
