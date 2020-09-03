3418c6d3cb65ca0cc5c593f97467124bab6f9c2b
Samir Isagov <samir.isagov@hotmail.com>
2020-09-04 00:57:56 +0400

    Add Get All and Get by id methods

---
+++ FitnessTracker.WebAPI/Controllers/BaseController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FitnessTracker.EFDataAccess.DataAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessTracker.WebAPI.Controllers
+{
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        private protected FitnessTrackerContext ctx;
+
+        public BaseController(FitnessTrackerContext ctx)
+        {
+            this.ctx = ctx;
+        }
+    }
+}
---
+++ FitnessTracker.WebAPI/Controllers/LibExercisesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FitnessTracker.EFDataAccess.DataAccess;
+using FitnessTracker.EFDataAccess.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FitnessTracker.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibExercisesController : BaseController
+    {
+        public LibExercisesController(FitnessTrackerContext ctx) : base(ctx) { }
+
+        // GET: api/<LibExercisesController>
+
+        [HttpGet]
+        public async Task<IEnumerable<LibExercise>> Get()
+        {
+            return await ctx.LibExercises.ToListAsync();
+        }
+
+        // GET api/<LibExercisesController>/5
+
+        [HttpGet("{id}")]
+        public async Task<LibExercise> Get(int id)
+        {
+            return await ctx.LibExercises.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        // POST api/<LibExercisesController>
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<LibExercisesController>/5
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LibExercisesController>/5
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
