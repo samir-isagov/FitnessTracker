5f1a3dc1094f8647db53ccd37644eb9597a1b92d
Samir Isagov <samir.isagov@hotmail.com>
2020-09-03 23:50:31 +0400

    Add initial migration

---
+++ FitnessTracker.EFDataAccess/Migrations/20200903194634_InitialCreate.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Oracle.EntityFrameworkCore.Metadata;
+
+namespace FitnessTracker.EFDataAccess.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LibExercises",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Oracle:ValueGenerationStrategy", OracleValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Calories = table.Column<int>(nullable: false),
+                    Duration = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LibExercises", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LibExerciseStates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Oracle:ValueGenerationStrategy", OracleValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LibExerciseStates", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Oracle:ValueGenerationStrategy", OracleValueGenerationStrategy.IdentityColumn),
+                    EmailAddress = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FinishedExercises",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Oracle:ValueGenerationStrategy", OracleValueGenerationStrategy.IdentityColumn),
+                    Duration = table.Column<int>(nullable: false),
+                    StateId = table.Column<int>(nullable: true),
+                    ExerciseId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FinishedExercises", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FinishedExercises_LibExercises_ExerciseId",
+                        column: x => x.ExerciseId,
+                        principalTable: "LibExercises",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_FinishedExercises_LibExerciseStates_StateId",
+                        column: x => x.StateId,
+                        principalTable: "LibExerciseStates",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FinishedExercises_ExerciseId",
+                table: "FinishedExercises",
+                column: "ExerciseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FinishedExercises_StateId",
+                table: "FinishedExercises",
+                column: "StateId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FinishedExercises");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "LibExercises");
+
+            migrationBuilder.DropTable(
+                name: "LibExerciseStates");
+        }
+    }
+}
